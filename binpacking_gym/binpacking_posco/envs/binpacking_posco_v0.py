e60a7f847c2b7ce2ae25149beb20f6fbc45b8f9b
lou9am <dd_special@naver.com>
2022-12-15 01:39:44 +0900

    v0

--- binpacking_gym/binpacking_posco/envs/binpacking_posco_v0.py
+++ binpacking_gym/binpacking_posco/envs/binpacking_posco_v0.py
@@ -3,72 +3,83 @@ import gym
 from gym import spaces
 from random import choice
 from copy import copy
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 """
-Loop를 통해 겹쳐지는 물건의 수를 제거하는 버전
-= 일정 수 이상 수를 못찾으면 종료되는 것으로.
+가장 간단한 환경
+Product : Random
 
-For stable-baseline,
-No masking
-When agent pick unavailable action, resample with While Loop.
+물리적으로 가능한 행동 : 1
+불가능 : -1
 """
 
 class binpacking_posco_v0(gym.Env):
     """
     Custom Env for 2D-bin Packing
-    
-    Reward = [0 - 100]
     """
     # Class Variables 
-    products = [(4, 4), (2, 2), (3, 3), (1, 1), (2, 2), (3, 3), (4, 4), (1, 1), (2, 2), (1, 1),
-                (4, 4), (2, 2), (3, 3), (1, 1), (2, 2), (3, 3), (4, 4), (1, 1), (2, 2), (1, 1),
-                (4, 4), (2, 2), (3, 3), (1, 1), (2, 2), (3, 3), (4, 4), (1, 1), (2, 2), (1, 1)]
-    reward_range = (0, 100)
+    products_list = [(1,1), (2,2), (3,3)] # for random sampling
+    
+    # reward_range = (0, 100)
     metadata = {'mode': ['human']}
     spec = "EnvSpec"
     
     
-    def __init__(self):
+    def __init__(self, **kwargs):
+        """
+        Kwargs / Default value (type)
+        -------------
+        ct2_threshold : 불가능한 행동을 몇번까지 허용할 것인가 / 50 (int)
+        # threshold 몇 %의 맵을 채웠을때 추가점수를 줄 것인가. : / 0.6 (float)
+        mapsize : 전체 맵 사이즈 / [10, 10] (list)
+        print_Map : Action시 마다 Map 출력 / True (bool)
+        """
         super(binpacking_posco_v0, self).__init__()
-        # Product's Size
-        # 여기서는 애초에 ㄱ자, ㄴ자 정의가 안됨
-        self.ct = 0 # index of products
-        self.width = self.products[self.ct][0]
-        self.length = self.products[self.ct][1]
+        # Params
+        self.ct2_threshold = kwargs.get('ct2_threshold', 20) # 불가능한 행동의 제한 수
+        self.mapsize = kwargs.get('mapsize', [10, 10])
+        self.print_Map = kwargs.get('print_Map', True)
+        self.threshold = kwargs.get('threshold', 0.6) # Default = 0.6 # 이 비율의 공간을 채웠을 때 더 많은 리워드를 줌
+        
+        # For ending of episode
+        self.filled_map = 0 
         self.ct2 = 0
+        
+        # Product's Size
+        self.random_product()
+        
         # Map of warehouse
-        self.Map = np.zeros([10, 10], dtype=int)
-        self.max_x = self.Map.shape[0]-1
-        self.max_y = self.Map.shape[1]-1
+        self.Map = np.zeros(self.mapsize, dtype=int)
+        self.max_x = self.mapsize[0]-1
+        self.max_y = self.mapsize[1]-1
+        
         self.state = None
-        
         self.actions_grid = [[i, j] for j in range (self.max_x+1) for i in range(self.max_y+1)]
-        #self.action_index = [i for i in range(len(self.actions_grid))] # Action's Index
         self.action_space = spaces.Discrete(len(self.actions_grid))
-        #self.action_space = spaces.MultiDiscrete([10, 10])
         
-        # Width == Length
-        low = np.array([0 for _ in range(len(self.actions_grid))] + [0])
+        # Observation space
+        ## Map + Max box size
+        low = np.array([0 for _ in range(len(self.actions_grid))] + [0]) 
         high = np.array([1 for _ in range(len(self.actions_grid))] + [4])
         self.observation_space = spaces.Box(low, high, dtype=int)
-        self.threshold = 0.6 # 이 비율의 공간을 채웠을 때 더 많은 리워드를 줌
-    
-    def update_product(self):
-        self.width = self.products[self.ct][0]
-        self.length = self.products[self.ct][1]
 
+    def random_product(self):
+        product = self.products_list[np.random.choice(len(self.products_list))]
+        self.width = product[0]
+        self.length = product[1]
+        self.filled_map += self.width * self.length
+    
     def int_action_to_grid(self, action):
         return (self.actions_grid[action])
     
     def available_act(self, action):
         """
         선택한 Action이 시행 가능한지 확인
-        """
-        action = self.int_action_to_grid(action)
-        
-        self.ct2 += 1
-        if action[0] + self.length > self.max_x:
+        """        
+        self.ct2 += 1 # count unavailable action
+        if action[0] + self.length > self.max_x + 1:
             return False
-        if action[1] + self.width > self.max_y:
+        if action[1] + self.width > self.max_y + 1:
             return False
         if self.Map[action[0]][action[1]] == 1:
             return False
@@ -80,59 +91,92 @@ class binpacking_posco_v0(gym.Env):
 
     def map_action(self, action):
         """
-        Randomly sample ACT with available act list.
-        
-        Actions of Map
-        1. Change Map (Drop product)
-        2. Change action_space
+        물건을 내려놓고 Map을 0 -> 1로 변경
         """        
         # Drop product (Only for Square) / Fill the Map
         self.Map[action[0]:(action[0] + self.length), action[1]:(action[1] + self.width)] = 1
 
     def step(self, action):
         action = self.int_action_to_grid(action)
-
+        
         terminated = bool(
-            self.ct2 == 30
+            self.ct2 == self.ct2_threshold
+            or self.filled_map > 80 # 80% 이상
         )
-
-        if not terminated: # 내려두지 않아야함 terminated이 True 일 때..
-            self.map_action(action)
-            self.ct += 1
-            self.update_product()
-            self.state = np.append(self.Map.flatten(), self.width) # 변경된 맵과 물건
-            reward = 1 # 물리적으로 올바른 행동을 했을 때 기본점수
-            self.ct2 = 0
+        
+        score = 0
+        if not terminated:
+            if self.available_act(action):
+                self.map_action(action)
+                self.random_product()
+                self.state = np.append(self.Map.flatten(), self.width)
+                reward = 1
+                self.ct2 = 0
+            else:
+                reward = -1
         else:
-            reward = self.calc_reward()
-            
-        return self.state, reward, terminated, {}
+            reward = 1
+        info = {'score' : score}
+        
+        return self.state, reward, terminated, info
 
     def reset(self):
         self.ct2 = 0
-        self.ct = 0 # index of products
-        self.width = self.products[self.ct][0]
-        self.length = self.products[self.ct][1]
+        self.filled_map = 0
+        
+        self.random_product()
+        
         # 매번 다른 state를 주고 학습 시킬 수 있음 !
         self.state = np.append(self.Map.flatten(), self.width)
         
         # Map of warehouse
-        self.Map = np.zeros([10, 10], dtype=int)
+        self.Map = np.zeros(self.mapsize, dtype=int)
         
         return np.array(self.state)
-    
-    def calc_reward(self):
-        score = self.Map.sum() / (self.Map.shape[0] * self.Map.shape[1])
-        if score > self.threshold:
-            return 100*score
-        else:
-            return score*5 # 행동이 끝났을 때 점수
-        
+
     def render(self, action, reward, mode='human'):
-        if mode == 'human':
-            print (f'Action :{action}, reward :{reward}, box :{self.products[self.ct-1]}')
-            print (self.Map)
-    
+        # if mode == 'human':
+        #     print (f'Action :{action}, reward :{reward}, box :{self.width, self.length}')
+        #     if self.print_Map:
+        #         print (self.Map)
+
+        iMode = plt.isinteractive()
+
+        if mode=='human':
+            plt.interactive(True)
+            # 1. 기본 스타일 설정
+        plt.style.use('default')
+        plt.rcParams['figure.figsize'] = (4, 4)
+        plt.rcParams['font.size'] = 7
+
+        # 2. 튜플의 리스트로 데이터 준비
+        Map = list(map(list, self.Map))
+        # Map에서 1인 부분 찾아 tuple의 list로 저장
+        boxes = []
+        for i in range(10) :
+            for j in range(10):
+                if Map[i][j] == 1:
+                    boxes.append((i, j, 1, 1))
+
+        # 3. 그래프 그리기
+        fig, ax = plt.subplots()
+
+        # Map
+        ax.set_yticks(np.arange(0, 11))
+        ax.set_xticks(np.arange(0, 11))
+        plt.grid(True)
+
+        # Plot Boxes
+        for (x, y, w, h) in boxes:
+            ax.add_patch(
+                patches.Rectangle(
+                    (x, y), w, h,
+                    edgecolor='green',
+                    facecolor='palegreen',
+                    fill=True))
+
+        plt.show()
+
     def close(self):
         pass
 
